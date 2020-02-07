20a98f554f5e891672e736246e5060a6de2a7f52
Tommi L. Jensen <toml@berlingskemedia.dk>
2020-02-07 10:25:54 +0100

    more s3 buckets

--- foo.tf
+++ foo.tf
@@ -17,3 +17,27 @@ resource "aws_vpc" "johnholm" {
   count = 0
   cidr_block = "192.127.129.0/24"
 }
+
+
+
+resource "aws_s3_bucket" "test2" {
+  bucket = "atlantis-poc-test-2"
+  acl = "private"
+  tags = {
+    Name = "test atlantis poc bucket"
+    Environment = "voliatile"
+  }
+}
+
+
+
+resource "aws_s3_bucket" "test3" {
+  bucket = "atlantis-poc-test-3"
+  acl = "private"
+  tags = {
+    Name = "test atlantis poc bucket"
+    Environment = "voliatile"
+  }
+}
+
+
