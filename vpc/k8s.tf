f23c344f01b842e7eb6d6d3a3976e3bb97566418
Tommi L. Jensen <toml@berlingskemedia.dk>
2020-02-19 15:10:53 +0100

    add name to vpc

--- vpc/k8s.tf
+++ vpc/k8s.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=0.8.1"
+  name       = "bemit-test-k8s"
   namespace  = "bemit"
   stage      = "test"
   name       = "k8s"
