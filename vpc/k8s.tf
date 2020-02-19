67efa7f113315ff78b74c248d2070956726c7d39
Tommi L. Jensen <toml@berlingskemedia.dk>
2020-02-19 16:01:02 +0100

    foo

--- vpc/k8s.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  count = 0
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.8.1"
-  namespace  = "bemit"
-  stage      = "test"
-  name       = "k8s"
-  cidr_block = "10.127.0.0/16"
-}
