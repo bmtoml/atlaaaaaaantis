4b6c6d0727875754bbbaaf9d65ae8888f1c00268
Tommi L. Jensen <toml@berlingskemedia.dk>
2020-02-20 09:55:29 +0100

    johnholm's k8s cluster

--- k8s-eks-test.tf
+++ k8s-eks-test.tf
@@ -1,12 +1,12 @@
-#module "test-k8s-cluster" {
-#  source    = "./modules/cp-eks"
-#  namespace = "bemit"
-#  stage     = "testing"
-#  region    = "eu-west-1"
-#  name      = "johnholm"
-#  tags      = {}
-#  providers = {
-#    aws = "aws"
-#  }
-#
-#}
+module "test-k8s-cluster" {
+  source    = "./modules/cp-eks"
+  namespace = "bemit"
+  stage     = "testing"
+  region    = "eu-west-1"
+  name      = "johnholm"
+  tags      = {}
+  providers = {
+    aws = "aws"
+  }
+
+}
