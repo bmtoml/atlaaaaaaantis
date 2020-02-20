88365e0413ff2ef67ff5c4e9b54e812201a2cda9
Tommi L. Jensen <toml@berlingskemedia.dk>
2020-02-20 09:06:14 +0100

    Revert "remove k8s cluster again"
    This reverts commit 4f95f96190f207de2021d8046b471783b143c872.

--- k8s-eks-test.tf
+++ k8s-eks-test.tf
@@ -1,8 +1,8 @@
-#module "test-k8s-cluster" {
-#  source    = "./modules/cp-eks"
-#  namespace = "bemit"
-#  stage     = "testing"
-#  region    = "eu-west-1"
-#  name      = "johnholm"
-#  tags      = {}
-#}
+module "test-k8s-cluster" {
+  source    = "./modules/cp-eks"
+  namespace = "bemit"
+  stage     = "testing"
+  region    = "eu-west-1"
+  name      = "johnholm"
+  tags      = {}
+}
