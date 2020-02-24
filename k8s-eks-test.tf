420c926046630fe2a50b9a3554135f180faf6afe
Tommi L. Jensen <toml@berlingskemedia.dk>
2020-02-24 16:56:20 +0100

    remove k8s clusters again

--- k8s-eks-test.tf
+++ k8s-eks-test.tf
@@ -1,22 +1,22 @@
-module "test-k8s-cluster" {
-  source    = "./modules/cp-eks"
-  namespace = "bemit"
-  stage     = "testing"
-  region    = "eu-west-1"
-  name      = "johnholm"
-  tags      = {}
-  providers = {
-    aws = "aws"
-  }
-}
-module "test-k8s-a" {
-  source    = "./modules/cp-eks"
-  namespace = "bemit"
-  stage     = "testing"
-  region    = "eu-west-1"
-  name      = "a"
-  tags      = {}
-  providers = {
-    aws = "aws"
-  }
-}
+#module "test-k8s-cluster" {
+#  source    = "./modules/cp-eks"
+#  namespace = "bemit"
+#  stage     = "testing"
+#  region    = "eu-west-1"
+#  name      = "johnholm"
+#  tags      = {}
+#  providers = {
+#    aws = "aws"
+#  }
+#}
+#module "test-k8s-a" {
+#  source    = "./modules/cp-eks"
+#  namespace = "bemit"
+#  stage     = "testing"
+#  region    = "eu-west-1"
+#  name      = "a"
+#  tags      = {}
+#  providers = {
+#    aws = "aws"
+#  }
+#}
